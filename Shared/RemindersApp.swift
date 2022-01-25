4bc69c3323fed1b5b9878f7a7e884b3c8db64cec
Sofia Andrade <sandrade@lcs.on.ca>
2022-01-25 08:44:09 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Sofia Andrade on 2022-01-25.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
